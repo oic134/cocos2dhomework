f27ef01b83d997400516b3f257e2b57b81c6906f
User <https://github.com/oic134/cocos2d.git>
2017-06-22 01:09:40 +0800

    170622test01

--- src/jimmy01.js
+++ src/jimmy01.js
@@ -4,17 +4,20 @@ var jimmy01Layer = cc.Layer.extend({
         var size = cc.winSize;
         var colorLayer = new cc.LayerColor(
               // 顏色             寬 高
-            cc.color(255,255,0), 320,96
+            cc.color(255,255,0), 192,55
         );
           colorLayer.x = size.width/2;
           colorLayer.y = size.height/2;
           colorLayer.ignoreAnchorPointForPosition(false); //預設座標轉換
           this.addChild(colorLayer);
         var myLabel =  new cc.LabelTTF("我的遊戲","Arial",48,
-            cc.size(320,96),
+            cc.size(192,55),
             cc.TEXT_ALIGNMENT_CENTER,
-            cc.VERTICAL_TEXT_ALIGNMENT_CENTER );
+            cc.VERTICAL_TEXT_ALIGNMENT_CENTER
+        );
 
+          var labelSize =  myLabel.getContentSize();
+          cc.log(labelSize);
           myLabel.x = size.width/2;
           myLabel.y = size.height/2 ;
           myLabel.setColor(cc.color(0,0,255));
